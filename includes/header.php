95eb96580822cd8f8df3c03e5ef9060a1907083b
PatriciaThereseDamaso <142222079+PatriciaThereseDamaso@users.noreply.github.com>
2025-06-29 23:48:12 +0800

    Split CSS files for better organization

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : 'User Authentication System'; ?></title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/base.css">
+    <link rel="stylesheet" href="../assets/css/split_layout.css">
+    <link rel="stylesheet" href="../assets/css/forms.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../assets/css/auth-branding.css">
+    <link rel="stylesheet" href="../assets/css/signup.css">
+    <link rel="stylesheet" href="../assets/css/animations.css">
+    <link rel="stylesheet" href="../assets/css/responsiveness.css">
     <script src="../assets/js/main.js" defer></script>
 </head>
 <body class="<?php echo isset($body_class) ? $body_class : ''; ?>"></body>
